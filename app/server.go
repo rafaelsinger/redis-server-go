828ff33427b2591dceb41431ed4c2113f9d85a1c
Rafael Singer <rafaelsinger66@gmail.com>
2024-09-14 21:27:13 -0400

    modularized server_test and handle new requests to server in a separate goroutine

--- app/server.go
+++ app/server.go
@@ -2,20 +2,49 @@ package app
 
 import (
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 func StartServer() {
-	ln, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Error starting TCP connection")
+		fmt.Println("Error starting TCP connection:", err)
+		os.Exit(1)
 	}
-	defer ln.Close()
+	defer listener.Close()
+	fmt.Println("Server is listening on port 6379...")
+
 	for {
-		conn, _ := ln.Accept()
-		go func(c net.Conn) {
-			conn.Write([]byte("+PONG\r\n"))
-			c.Close()
-		}(conn)
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Client closed connection")
+				return
+			}
+			fmt.Println("Error reading connection:", err)
+			return
+		}
+		buffer = buffer[:n]
+		fmt.Printf("Received %s", string(buffer))
+
+		// r := bytes.NewReader(buffer)
+		// TODO: read bytes to handle multiple requests from the same connection
+
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
--- app/server_test.go
+++ app/server_test.go
@@ -6,23 +6,38 @@ import (
 	"testing"
 )
 
-func TestPing(t *testing.T) {
+func sendRedisCommand(t *testing.T, commands []string) []byte {
 	conn, err := net.Dial("tcp", "0.0.0.0:6379")
 	if err != nil {
 		t.Fatalf("Unable to connect to Redis server: %v", err)
 	}
 	defer conn.Close()
-	conn.Write([]byte("+PING\r\n"))
+	for _, command := range commands {
+		conn.Write([]byte(command + "\r\n"))
+	}
 
 	var output = make([]byte, 2048)
 	num_bytes, err := conn.Read(output)
 	if err != nil {
 		t.Fatalf("Error reading from Redis: %v", err)
 	}
-	output = output[:num_bytes]
+	return output[:num_bytes]
+}
 
-	expected := "+PONG\r\n"
-	if !bytes.Equal(output, []byte(expected)) {
-		t.Fatalf("Expected %s, received %s", expected, output)
+func testBytesEquality(t *testing.T, expected, received []byte) {
+	if !bytes.Equal(expected, received) {
+		t.Fatalf("Expected %s, received %s", expected, received)
 	}
+}
+
+func TestPing(t *testing.T) {
+	// single ping
+	output := sendRedisCommand(t, []string{"*1\r\n$4\r\nPING\r\n"})
+	expected := []byte("+PONG\r\n")
+	testBytesEquality(t, expected, output)
+
+	// multiple pings in the same connection
+	output = sendRedisCommand(t, []string{"*1\r\n$4\r\nPING\r\n", "*1\r\n$4\r\nPING\r\n"})
+	expected = []byte("+PONG\r\n+PONG\r\n")
+	testBytesEquality(t, expected, output)
 }
