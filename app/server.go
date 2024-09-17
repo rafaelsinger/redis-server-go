2e8f2e82bb15abdcf33bccf972f5f71e9e145d9c
Rafael Singer <rafaelsinger66@gmail.com>
2024-09-17 11:27:35 -0400

    starting improvement of RESP parsing

--- app/server.go
+++ app/server.go
@@ -1,10 +1,14 @@
 package app
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func StartServer() {
@@ -40,11 +44,67 @@ func handleRequest(conn net.Conn) {
 			return
 		}
 		buffer = buffer[:n]
-		fmt.Printf("Received %s", string(buffer))
+		parseRESP(buffer, conn)
+	}
+}
 
-		// r := bytes.NewReader(buffer)
-		// TODO: read bytes to handle multiple requests from the same connection
+func parseRESP(request []byte, conn net.Conn) {
+	// RESP arrays format: *<number-of-elements>\r\n<element-1>...<element-n>
 
+	// initialize using conn?
+	reader := bufio.NewReader(bytes.NewReader(request))
+	for {
+		// read until the first delimiter to get the number of elements
+		_, err := reader.ReadString(byte('\n'))
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			fmt.Println("Error parsing request:", err)
+			return
+		}
+		for {
+			data, err := reader.ReadString(byte('\n'))
+			// remove delimiter
+			data = strings.TrimSuffix(data, "\r\n")
+			if err != nil {
+				break
+			}
+			if data == "" {
+				break
+			}
+			dataLength, err := strconv.Atoi(data[1:])
+			if err != nil {
+				fmt.Println("Error processing data length", err)
+			}
+			switch data[0] {
+			case '+':
+				// TODO
+			case '$':
+				parseBulkString(reader, dataLength, conn)
+			case ':':
+				//TODO
+			default:
+				fmt.Println("Unknown request type", data)
+			}
+		}
+	}
+}
+
+func parseBulkString(reader io.Reader, dataLength int, conn net.Conn) {
+	dataBuffer := make([]byte, dataLength)
+	n, err := io.ReadFull(reader, dataBuffer)
+	if err != nil {
+		fmt.Println("Error reading data", err)
+	}
+	if n < dataLength {
+		fmt.Printf("Unable to parse full bulk string. Expected %d, read %d", dataLength, n)
+	}
+	data := string(dataBuffer)
+	switch data {
+	case "PING":
 		conn.Write([]byte("+PONG\r\n"))
+	default:
+		fmt.Println("Unknown simple string command")
 	}
 }
